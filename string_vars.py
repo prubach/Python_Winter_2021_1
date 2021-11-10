4b9444f8cec42de6908556f703b5ee06ed3e2503
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-11-10 14:39:24 +0100

    String operations

---
+++ string_vars.py
@@ -0,0 +1,16 @@
+
+if __name__ == '__main__':
+    s = 'abcdefghiajklam'
+    print(len(s))
+    print(s[1:5])
+    # first 7 letters
+    print(s[:7])
+    # last 7 letters
+    print(s[-7:])
+    print(s[-7:-4])
+    print(s.count('a'))
+    print(s.index('a'))
+    print(s.rindex('a'))
+
+    print('----------------------------')
+
