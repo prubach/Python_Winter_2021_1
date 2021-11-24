ad222cc607f51e339a349f0f3343bfb3e4315d1b
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-11-24 15:06:46 +0100

    Lists and dictionaries

---
+++ dicts.py
@@ -0,0 +1,6 @@
+d1 = {'abc': 3636, 'def': 667547}
+print(d1)
+print(d1['abc'])
+print(d1.keys())
+for k in d1.keys():
+    print('{}:{}'.format(k, d1[k]))
--- lists.py
+++ lists.py
@@ -25,6 +25,31 @@ l4 = [3636, 78785, 1526, 255.453, 'Hello Python 3', 75675]
 print(l4)
 
 # Extract the word 'Python' from l4
+print(l4[4][6:12])
+print(l4[4].split(' ')[1])
 
 
 # Sum all number values of l4
+l5 = l4[0:4] + l4[5:]
+print(sum(l5))
+
+my_sum = 0
+for el in l4:
+    if type(el)==int or type(el)==float:
+        my_sum = my_sum + el
+print(my_sum)
+l6 = [el for el in l4 if type(el)==int or type(el)==float]
+
+sum_l6 = sum([el for el in l4 if type(el)==int or type(el)==float])
+print(l6)
+print(sum_l6)
+
+
+l1 = [4232, 245326, 57467347, 362363, 36364, 346123782]
+l1i = [el*2 for el in l1]
+print(l1i)
+
+l1ii = []
+for el in l1:
+    l1ii.append(el*2)
+print(l1ii)
