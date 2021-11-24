0a8c1e7c375dcf630a0fb88bc38ed86616dec713
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-11-24 14:30:24 +0100

    Lists

---
+++ lists.py
@@ -0,0 +1,30 @@
+l1 = [4232, 245326, 57467347, 362363, 36364, 346123782]
+
+print(l1[0])
+print(l1[5])
+print(l1[-1])
+
+l2 = l1[1:5]
+print(l2)
+l2.append(68979)
+print(l2)
+l2.insert(2, 966743)
+l2[1] = 5723
+print(l2)
+
+l3 = l2
+l3.sort()
+print(l3)
+l3.sort(reverse=True)
+print(l3)
+
+print('--------------')
+print('')
+
+l4 = [3636, 78785, 1526, 255.453, 'Hello Python 3', 75675]
+print(l4)
+
+# Extract the word 'Python' from l4
+
+
+# Sum all number values of l4
