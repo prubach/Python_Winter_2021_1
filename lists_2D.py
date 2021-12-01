86fe065207aa588dbaa3fda6bb2ea5f907f85b5e
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-12-01 13:47:57 +0100

    Sum matrix - exercise

--- lists_2D.py
+++ lists_2D.py
@@ -1,3 +1,10 @@
-l2d = [[5, 5, 7], [9, 6, 4], [9, 14, 4]]
+l2d = [[5, 5, 7],
+       [9, 6, 4],
+       [9, 14, 4],
+       [1, 4, 5]]
 print(l2d[1][2])
 
+#TODO
+# 1. sum the rows of l2d
+# 2. sum the columns of l2d [24, 29, 20]
+# 3. sum all elements of l2d
